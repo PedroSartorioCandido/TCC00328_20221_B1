10adf4efcfc3a74fa78f5f4622723354e0693c1f
PedroSartorioCandido <86096505+PedroSartorioCandido@users.noreply.github.com>
2022-04-10 11:34:07 -0300

    Initial commit

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20212/exercicio/exercicio1/Conversao.java
@@ -0,0 +1,18 @@
+package uff.ic.lleme.tcc00328.s20212.exercicio.exercicio1;
+
+public class Conversao {
+
+    public static float cels;/* armazena temperatura em oC */
+    public static float fahr;/* armazena temperatura em oF */
+
+    public static void main(String[] args) {
+        int n, x;
+        n = 5;
+        x = n++;
+        System.out.println("" + x);
+        x = ++n;
+        System.out.println("" + x);
+
+    }
+
+}
